e6dfd933db1a67262a6eefb5098558c22d05ef53
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2022-02-01 11:17:40 +0900

    Deviseを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  root to: 'posts#index'
   get 'comments/index'
   get 'goods/index'
   get 'follows/index'
