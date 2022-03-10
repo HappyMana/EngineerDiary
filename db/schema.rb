3184262015f10d3b05147b2d572ee61e7c5de244
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2022-03-10 13:15:48 +0900

    スキーマを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_052539) do
+ActiveRecord::Schema.define(version: 2022_02_13_151435) do
 
-  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "comment", default: "", null: false
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "goods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_goods_on_post_id"
-    t.index ["user_id"], name: "index_goods_on_user_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "text", default: "", null: false
-    t.string "tag", default: ""
-    t.boolean "public", default: true, null: false
+    t.string "title", default: "", null: false
+    t.string "text", default: ""
+    t.string "url", default: "", null: false
+    t.bigint "tag_id"
+    t.boolean "is_public", default: true, null: false
+    t.boolean "is_read", default: true, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_posts_on_tag_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -60,9 +54,8 @@ ActiveRecord::Schema.define(version: 2022_02_13_052539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "goods", "posts"
-  add_foreign_key "goods", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "tags"
   add_foreign_key "posts", "users"
 end
