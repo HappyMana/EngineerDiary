ae9453e86b3fec86af8450f29f2dec05b2d31daf
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2022-03-30 17:33:54 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,52 @@
 class UsersController < ApplicationController
+  def likes
+    @posts = Post.joins(:likes).where(user_id: current_user.id).page(params[:page]).per(9)
+  end
+
   def show
-    if user_signed_in?
-      @user = User.find(current_user.id)
-      @posts = Post.where(user_id: current_user.id)
+    @user = User.find(params[:id])
+    p params[:id]
+    if @user.id == current_user.id
+      @posts = Post.where(user_id: @user.id).order("created_at DESC").page(params[:page]).per(9)
+      p "ユーザー自身のとき"
     else
-      redirect_to new_user_session_path
+      @posts = Post.where(user_id: @user.id, is_public: true).page(params[:page]).per(9)
+      p "ユーザー自身じゃないとき"
     end
+  end
+
+  # def update
+  #   @user = User.find(current_user)
+  #   @user.update(update_params)
+  #   redirect_to user_path(@user.id)
+  # end
+
+  def search
+    # タグ検索
+    if params[:tag_id] === ""
+      @posts = Post.where(user_id: current_user.id).order("created_at DESC").page(params[:page]).per(9)
+    else
+      @posts = Post.where(tag_id: params[:tag_id], user_id: current_user.id).limit(100).order("created_at DESC").page(params[:page]).per(9)
+    end
+    # 読んだか読んでないか
+    # if params[:is_read] === true
+    #   @posts = Post.where(is_read: true, user_id: current_user.id).limit(100).order("created_at DESC").page(params[:page]).per(9)
+    # else
+    #   @posts = Post.where(is_read: false, user_id: current_user.id).limit(100).order("created_at DESC").page(params[:page]).per(9)
+    # end
+    # 並べ順検索
+    # case params[:sort_id]
+    # when "desc" then
+    #   @posts.order("created_at DESC")
+    # when "like" then
+    # else
+    # end
+    render action: :show
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name, :user_img)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-
   resources :posts do
     resource :likes, only: [:create, :destroy]
     collection do
@@ -11,7 +10,13 @@ Rails.application.routes.draw do
     end
   end
   resources :tags, only: [:new, :create]
-  resources :users, only: [:show]
+  resources :users do
+    collection do
+      get 'likes'
+      get 'search'
+      get 'update'
+    end
+  end
   root 'posts#index'
   # get '/search' to 'posts#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
