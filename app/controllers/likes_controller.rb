c1f1ec3ce129e2c027f820a757fdb9e378bbaef4
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2022-05-05 17:59:05 +0900

    [fix] いいねをprivateの関数に移動

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,22 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.new({post_id: params[:post_id], user_id: current_user.id})
+    @like = Like.new(like_params)
     if @like.save
       redirect_to request.referer
     else
-      render root_path
+      flash.now[:alert] = 'いいねに失敗しました'
+      render request.referer
     end
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id).destroy
     redirect_to request.referer
+  end
+
+  private
+  def like_params
+    params.permit(:post_id).merge(user_id: current_user.id)
   end
 end
